924c9f89d4c102b169389970f411448c0768a125
mod728 <vinnybeach712@yahoo.co.jp>
2022-04-25 13:29:33 +0900

    Code: Chapter 6-9

---
+++ pages/index.js
@@ -0,0 +1,36 @@
+import Link from "next/link"
+import Image from "next/image"
+import Head from "next/head"
+
+const ReadAllItems = (props) => {
+  return (
+    <div>
+        <Head><title>Next Market</title></Head>
+        <div className="grid-container-in">
+        {props.allItems.map(item => 
+            <Link href={`/item/${item._id}`} key={item._id}> 
+              <a className="card">
+                  <Image src={item.image} width="750px" height="500px" alt="item-image"/>
+                  <div className="texts-area">
+                      <h2>¥{item.price}</h2>
+                      <h3>{item.title}</h3>
+                      <p>{item.description.substring(0, 80)}...</p>
+                  </div>
+              </a>
+            </Link>
+        )}
+        </div>
+    </div>
+  )
+}
+
+export default ReadAllItems
+
+export const getServerSideProps = async() => {
+  const response = await fetch("https://nextjs-book-fullstack-app.vercel.app/api/item/readall")
+  const allItems = await response.json()
+
+  return{
+    props: allItems 
+}
+}
